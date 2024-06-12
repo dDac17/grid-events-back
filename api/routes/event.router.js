6c02e20f6b326d438693378cda3397b70189e85e
Diego <dDac17@github.com>
2024-06-12 21:43:03 +0100

    event pendiente de test las rutas

--- api/routes/event.router.js
+++ api/routes/event.router.js
@@ -15,13 +15,14 @@ const {
 } = require('../middelwares')
 
 //revisar checkAuth cuándo esten todas las rutas hechas!!!
+// pendiente test de añadir fav y like
 
 router.post('/', createEvent)
 router.get('/', getAllEvents)
 router.get('/:id', checkAuth, getOneEvent)
-router.post('/:id/favorite', checkAuth, addFavouriteEvent)
+router.post('/:id/favorite', addFavouriteEvent)
 router.put('/:id/like', addLike)
-router.delete('/:id',checkAuth, deleteEvent)
-router.put('/:id', checkAuth, updateEvent)
+router.delete('/:id', deleteEvent)
+router.put('/:id', updateEvent)
 
 module.exports = router
